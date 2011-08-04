1f3bea5c9620628474f16fab9bc0a4963caa62bb
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-08-04 17:45:31 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 METADATA = dict(
     name='django-riak-sessions',
-    version="0.0.2",
+    version="0.0.3",
     author='Alen Mujezinovic',
     author_email='flashingpumpkin@gmail.com',
     description='Riak session backend for django',
